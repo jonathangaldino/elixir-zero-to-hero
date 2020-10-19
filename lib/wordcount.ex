52c5bb13acb6c6cee04f8ec2ffa04f9d7a6b0af6
Jonathan <johnzballad@gmail.com>
2020-10-19 12:52:17 -0300

    Wordcount algorithm

---
+++ lib/wordcount.ex
@@ -0,0 +1,24 @@
+defmodule Wordcount do
+  @moduledoc """
+  Provides a function to count the occurrences of each word in that phrase
+
+  ## Examples
+
+    iex> Wordcount.count("olly olly in come free")
+    "olly: 2
+    in: 1
+    come: 1
+    free: 1"
+  """
+  @spec count(String) :: String
+  def count(word) do
+    word
+    |> String.split(" ")
+    |> Enum.reduce(%{}, fn w, acc ->
+      case acc do
+        %{^w => count} -> %{acc | w => count + 1}
+        _ -> Map.put(acc, w, 1)
+      end
+    end)
+  end
+end
