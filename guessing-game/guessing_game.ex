ade0a4149b297cdaa43784ce5a5904c0880b8904
Jonathan <johnzballad@gmail.com>
2020-10-19 01:03:21 -0300

    documentation

--- guessing-game/guessing_game.ex
+++ guessing-game/guessing_game.ex
@@ -1,6 +1,30 @@
 defmodule GuessingGame do
+  @moduledoc """
+  Provides a guessing game by using `guess/2` to guess a number
+  """
+
   def guess(a, b) when a > b, do: guess(b, a)
 
+  @doc """
+  Guess a number between low and high
+
+  ## Parameters
+
+    - low: Integer that represents the lowest the guessed number can be
+    = high: Integer that represents the highest the guessed number can be
+
+  ## Examples
+
+    iex> GuessingGame.guess(1, 50)
+    "Hm... maybe you're thinking of 30?"
+    bigger
+    Hm... maybe you're thinking of 42?
+    smaller
+    Hm... maybe you're thinking of 35?
+    yes
+    I knew I could guess your number.
+  """
+  @spec guess(integer, integer) :: integer
   def guess(low, high) do
     answer = IO.gets("Hmm... maybe you're thinking of #{mid(low, high)}?\n")
 
@@ -14,10 +38,12 @@ defmodule GuessingGame do
     end
   end
 
+  @spec mid(integer, integer) :: integer
   def mid(low, high) do
     div(low + high, 2)
   end
 
+  @spec bigger(integer, integer) :: integer
   def bigger(low, high) do
     new_low = min(high, mid(low, high) + 1)
     guess(new_low, high)
